ed54e3925aba8a8f97db995f0fcf8cded6e0e425
uzifauzi009 <fauziakamal8167@gmail.com>
2026-08-29 14:05:20 +0700

    update lagi

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
-app.listen(3000, () => {
-    console.log('Server berjalan di http://localhost:3000');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
 });
